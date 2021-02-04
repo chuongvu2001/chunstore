95249656d123c176eeee89a29ffbe09f97c751f4
chuongvu2001 <63601506+chuongvu2001@users.noreply.github.com>
2021-02-04 16:28:47 +0700

    Update Category.php

--- app/models/Category.php
+++ app/models/Category.php
@@ -3,4 +3,7 @@
  use Illuminate\Database\Eloquent\Model;
  class Category extends Model{
     protected $table = "categories";
+    public function get_child(){
+        return $this->hasMany(Category::class,'id');
+    }
  }
